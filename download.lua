8088a751ab220b53fc29f586d5b4f29abbe18a5a
CoffeeCatRailway <dragonlee1002@gmail.com>
2020-12-16 10:37:35 +1100

    download

---
+++ download.lua
@@ -0,0 +1,5 @@
+local os = require("os")
+
+print("Downloading files...")
+os.execute("wget https://github.com/CoffeeCatRailway/displaySign/List.lua")
+os.execute("wget https://github.com/CoffeeCatRailway/displaySign/main.lua")
