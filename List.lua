f2d9eb941a382abd74180a8bec1aa75f780c4cd0
CoffeeCatRailway <dragonlee1002@gmail.com>
2020-12-16 10:36:34 +1100

    List

---
+++ List.lua
@@ -0,0 +1,66 @@
+-- Initialization
+local List = {size = 1, array = {}, default = nil}
+
+local function initArray(diff, default)
+	-- Initialize self.array
+	self.default = default
+	self.array = {}
+	local newSize = self.size + diff
+	for i = 1,newSize,1 do
+		self.array[i] = self.default
+	end
+	return self.array
+end
+
+function List:new(o, size, default)
+	o = o or {}
+	setmetatable(o, self)
+	self.__index = self
+	self.size = size or 1
+	self.array = initArray(0, default or nil)
+	return o
+end
+
+-- Methods
+function List:add(object, forceIn)
+	-- Add element to array
+	local force = forceIn or true
+	local index = 0;
+
+	-- Add if has empty space
+	for i = 1,self.size,1 do
+		if self.array[i] == nill then
+			self.array[i] = object
+		end
+		index = index + 1
+	end
+
+	-- Add element to end of array
+	if force and index == self.size then
+		local oldArray = self.array
+		local newArray = initArray(1, self.default)
+
+		for i = 1,self.size,1 do
+			newArray[i] = oldArray[i]
+		end
+
+		self.size = self.size + 1
+		newArray[self.size] = object
+		self.array = newArray
+	end
+end
+
+function List:get(index)
+	-- Get element in array
+	assert(type(index) == "number", "Index '" .. tostring(index) .. "' must be a integer!")
+	assert(index >= 1 or index <= self.size, "Index out of bounds! [1," .. tostring(self.size) .. "]")
+	return self.array[index]
+end
+
+function List:getSize()
+	-- Get size of array
+	return self.size
+end
+
+-- Return
+return List
---
+++ main.lua
@@ -0,0 +1,8 @@
+local list = require("List")
+
+local newList = List:new(nil, 1, 0) -- Create new List
+newList:add(2) -- Add value 2
+print(newList:get(1)) -- Get value 2 in first slot
+
+newList:add(3) -- Add value 3 to end of array, note: array size is 1
+print(newList:get(2)) -- Get value 3 in second slot
