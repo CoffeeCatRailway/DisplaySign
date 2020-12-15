10b436d6e22575a25094d9ae83a171c7255afde6
CoffeeCatRailway <dragonlee1002@gmail.com>
2020-12-16 10:57:19 +1100

    Update List.lua

--- List.lua
+++ List.lua
@@ -18,48 +18,49 @@ function List:new(o, size, default)
 	self.__index = self
 	self.size = size or 1
 	self.array = initArray(0, default or nil)
-	return o
-end
 
--- Methods
-function List:add(object, forceIn)
-	-- Add element to array
-	local force = forceIn or true
-	local index = 0;
+	-- Methods
+	function List:add(object, forceIn)
+		-- Add element to array
+		local force = forceIn or true
+		local index = 0;
 
-	-- Add if has empty space
-	for i = 1,self.size,1 do
-		if self.array[i] == nill then
-			self.array[i] = object
-		end
-		index = index + 1
-	end
-
-	-- Add element to end of array
-	if force and index == self.size then
-		local oldArray = self.array
-		local newArray = initArray(1, self.default)
-
+		-- Add if has empty space
 		for i = 1,self.size,1 do
-			newArray[i] = oldArray[i]
+			if self.array[i] == nill then
+				self.array[i] = object
+			end
+			index = index + 1
 		end
 
-		self.size = self.size + 1
-		newArray[self.size] = object
-		self.array = newArray
+		-- Add element to end of array
+		if force and index == self.size then
+			local oldArray = self.array
+			local newArray = initArray(1, self.default)
+
+			for i = 1,self.size,1 do
+				newArray[i] = oldArray[i]
+			end
+
+			self.size = self.size + 1
+			newArray[self.size] = object
+			self.array = newArray
+		end
 	end
-end
 
-function List:get(index)
-	-- Get element in array
-	assert(type(index) == "number", "Index '" .. tostring(index) .. "' must be a integer!")
-	assert(index >= 1 or index <= self.size, "Index out of bounds! [1," .. tostring(self.size) .. "]")
-	return self.array[index]
-end
+	function List:get(index)
+		-- Get element in array
+		assert(type(index) == "number", "Index '" .. tostring(index) .. "' must be a integer!")
+		assert(index >= 1 or index <= self.size, "Index out of bounds! [1," .. tostring(self.size) .. "]")
+		return self.array[index]
+	end
 
-function List:getSize()
-	-- Get size of array
-	return self.size
+	function List:getSize()
+		-- Get size of array
+		return self.size
+	end
+	
+	return o
 end
 
 -- Return
