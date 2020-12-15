7673703cc32fbd382f57d832a1950b2164f2fa7e
CoffeeCatRailway <dragonlee1002@gmail.com>
2020-12-16 10:51:38 +1100

    Update downloadDSFiles.lua

--- downloadDSFiles.lua
+++ downloadDSFiles.lua
@@ -4,5 +4,5 @@ os.execute("del List.lua")
 os.execute("del main.lua")
 
 print("Downloading files...")
-os.execute("wget https://github.com/CoffeeCatRailway/displaySign/blob/main/List.lua")
-os.execute("wget https://github.com/CoffeeCatRailway/displaySign/blob/main/main.lua")
+os.execute("wget https://raw.githubusercontent.com/CoffeeCatRailway/displaySign/main/List.lua")
+os.execute("wget https://raw.githubusercontent.com/CoffeeCatRailway/displaySign/main/main.lua")
