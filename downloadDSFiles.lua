dbd86336f92b9069d882767a7c4a98df81f9bac8
CoffeeCatRailway <dragonlee1002@gmail.com>
2020-12-16 10:43:39 +1100

    Rename download to downloadDSFiles

--- downloadDSFiles.lua
+++ downloadDSFiles.lua
@@ -1,5 +1,8 @@
 local os = require("os")
 
+os.execute("del List.lua")
+os.execute("del main.lua")
+
 print("Downloading files...")
 os.execute("wget https://github.com/CoffeeCatRailway/displaySign/List.lua")
 os.execute("wget https://github.com/CoffeeCatRailway/displaySign/main.lua")
