5300f2788efa729504455d4ef162919ee4ad7698
CoffeeCatRailway <dragonlee1002@gmail.com>
2020-12-16 10:45:51 +1100

    Update downloadDSFiles.lua

--- downloadDSFiles.lua
+++ downloadDSFiles.lua
@@ -4,5 +4,5 @@ os.execute("del List.lua")
 os.execute("del main.lua")
 
 print("Downloading files...")
-os.execute("wget https://github.com/CoffeeCatRailway/displaySign/List.lua")
-os.execute("wget https://github.com/CoffeeCatRailway/displaySign/main.lua")
+os.execute("wget https://github.com/CoffeeCatRailway/displaySign/blob/main/List.lua")
+os.execute("wget https://github.com/CoffeeCatRailway/displaySign/blob/main/main.lua")
